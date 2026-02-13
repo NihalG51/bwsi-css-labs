f57460637d0d5e9d50c82c4b5c59188416e5ebd1
NihalG51 <nihalg087@gmail.com>
2026-02-13 10:13:55 -0800

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Nihal" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("I like to play the violin")
 if __name__ == "__main__":
     main()
